5720e29657b4072608b781a4970ba0c51a32941b
Sarang K <sarang@bloomcs.com>
2017-10-04 16:34:28 +0530

    Added DateTimeHelper.Portable.Tests project

---
+++ DateTimeHelper/DateTimeHelper.Portable.Tests/DateTimeExtensionsUnitTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using DateTimeHelper.Portable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DateTimeHelper.Portable.Tests
+{
+    [TestClass]
+    public class DateTimeExtensionsUnitTests
+    {
+        readonly DateTime dateTime = new DateTime(2017, 10, 12, 12, 10, 01);
+
+        [TestMethod]
+        public void ToUnixTimeStampTest1()
+        {
+            var result = dateTime.ToUnixTimeStamp();
+            var expectedDate = DTHelper.UnixTimeStampToDateTime(result);
+            Assert.AreEqual(0, dateTime.CompareTo(expectedDate));
+        }
+
+        [TestMethod]
+        public void ToStringIsoTest()
+        {
+            var result = dateTime.ToStringIso();
+            Assert.AreEqual("2017-10-12 12:10:01", result);
+        }
+
+        [TestMethod]
+        public void GetFirstDayOfTheMonthTest()
+        {
+            var result = dateTime.GetFirstDayOfTheMonth();
+            var expectedDate = new DateTime(2017, 10, 01, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void GetLastDayOfTheMonthTest()
+        {
+            var result = dateTime.GetLastDayOfTheMonth();
+            var expectedDate = new DateTime(2017, 10, 31, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void GetFirstDayOfWeekTest1()
+        {
+            var result = dateTime.GetFirstDayOfWeek(DayOfWeek.Monday);
+            var expectedDate = new DateTime(2017, 10, 09, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void GetFirstDayOfWeekTest2()
+        {
+            var result = dateTime.GetFirstDayOfWeek(DayOfWeek.Sunday);
+            var expectedDate = new DateTime(2017, 10, 08, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void GetLastDayOfWeekTest1()
+        {
+            var result = dateTime.GetLastDayOfWeek(DayOfWeek.Monday);
+            var expectedDate = new DateTime(2017, 10, 15, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void GetLastDayOfWeekTest2()
+        {
+            var result = dateTime.GetLastDayOfWeek(DayOfWeek.Sunday);
+            var expectedDate = new DateTime(2017, 10, 14, 00, 00, 00);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }
+
+        [TestMethod]
+        public void ToTimeZoneTest()
+        {
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time"); // (UTC+08:00) Kuala Lumpur, Singapore
+            var result = dateTime.ToTimeZone(timeZoneInfo);
+            var expectedDate = dateTime.ToUniversalTime().AddHours(8);
+            Assert.AreEqual(0, expectedDate.CompareTo(result));
+        }        
+    }
+}
